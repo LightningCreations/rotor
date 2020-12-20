a62a6ccf568ea123d1373dcd66108895c38896d7
moonheart08 <moonheart08@users.noreply.github.com>
2020-12-20 13:17:08 -0600

    codify behavior of some variables, add aug stuff and flags.

--- p2/src/cog.rs
+++ p2/src/cog.rs
@@ -1,4 +1,5 @@
 use crate::hub::Hub;
+use std::num::NonZeroU32;
 
 pub struct P2Cog {
     /// Cog RAM, aka the general purpose registers.
@@ -15,12 +16,20 @@ pub struct P2Cog {
     // CONSIDER: Maybe using decoding_instr to do decode in advance? This is probably slower than normal execution with how simple P2 encoding is though.
     _executing_instr: (u32, Option<u32>),
 
+    /// Program counter
     _pc: u32,
+
+    /// Zero flag
+    _z: bool,
+    /// Carry flag
+    _c: bool,
 
     /// Altered D register, if set by the last executed instr (ALT* class)
     _alt_d: Option<u32>,
+    _aug_d: Option<u32>,
     /// Altered S register, if set by the last executed instr (ALT* class)
     _alt_s: Option<u32>,
+    _aug_s: Option<u32>,
     /// Altered R register, if set by the last executed instr (ALT* class)
     _alt_r: Option<u32>,
 
@@ -31,9 +40,8 @@ pub struct P2Cog {
     _alt_i: Option<(u32, u32)>,
 
     // need to include stuff some of the other ALT class instrs use, but idk what they need as of now. --moony
-    _skip_mask: u32,
-    _skip_address: u32,
-    _skip_type: SkipType,
+    /// Controls SKIP, SKIPF, and EXECF behavior. (Mask, Address, Type)
+    _skip_ctrl: Option<(u32, u32, SkipType)>,
 
     /// The `Q` register.
     /// Somehow, for some reason, SETQ and SETQ2 modify the behavior of the subsequent instructions in some cases, hence the bool to differentiate the two.
@@ -42,11 +50,8 @@ pub struct P2Cog {
     /// Interrupt enable.
     _interrupt_en: bool,
 
-    /// Repetition counter, used by REP instruction.
-    /// Should be zeroed alongside rep_end if the P2 ever takes a branch.
-    _rep_loop_counter: u32,
-    /// Address of instruction REP will decrement the loop counter on.
-    _rep_end: u32,
+    /// Repetition counter and end address, used by REP instruction. (Counter, End Address)
+    _rep_ctrl: Option<(NonZeroU32, u32)>,
 
     /// Set by the SETLUTS instruction. If set, any writes to LUTRAM should also write to LUTRAM on adjacent cog, this is done through the Hub.
     _lut_sharing_enabled: bool,
